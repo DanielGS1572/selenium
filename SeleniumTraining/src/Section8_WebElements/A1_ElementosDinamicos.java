37df4d807d82842679b22ff8343d0aea9e85154e
DanielGS1572 <gonzalisd@gmail.com>
2019-05-09 22:02:34 -0500

    selenium

--- SeleniumTraining/src/Section8_WebElements/A1_ElementosDinamicos.java
+++ SeleniumTraining/src/Section8_WebElements/A1_ElementosDinamicos.java
@@ -22,15 +22,35 @@ public class A1_ElementosDinamicos {
 		driver.get("http://spicejet.com");
 		driver.findElement(By.linkText("Hotels")).click();
 		
-		Select s = new Select(driver.findElement(By.id("ddl_Adult_MST")));			//adentro debe tener <option ...>
+		Select s = new Select(driver.findElement(By.id("ddl_Adult_MST")));			//adentro debe tener <option ...> el cual no se toma en cuenta
 		//Porque esto no funciona?
 		//Select s = new Select(driver.findElement(By.cssSelector(".wMed1s.required.valid")));
 		
 		s.selectByValue("2"); 		
 		s.selectByIndex(4); 		
 		s.selectByVisibleText("6"); 		
+		driver.findElement(By.linkText("Flights")).click();
 		
+	driver.manage().window().maximize();
 		
+		driver.findElement(By.id("divpaxinfo")).click();
+		Thread.sleep(2000L);
+		int i =0;
+		while(i<5){			
+			driver.findElement(By.id("hrefIncAdt")).click();
+			i++;
+		}
+		
+		driver.findElement(By.id("btnclosepaxoption")).click();
+		
+/*Hay veces en las que los dropdowns se cargan nada más cuando se abre un combobox, por ejemplo
+ * entrar a https://www.spicejet.com/ y en From esta chennai, al igual que en el To... por lo que 
+ * con xpath para encontrar al segundo chennai sería (//a[@value='MAA'])[2] 	
+ */
+		driver.findElement(By.id("ctl00_mainContent_ddl_originStation1_CTXT")).click();
+		driver.findElement(By.xpath("//a[@value='BLR']")).click();
+		Thread.sleep(2000L);
+		driver.findElement(By.xpath("(//a[@value='MAA'])[2]")).click();
 	}
 
 }

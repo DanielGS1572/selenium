cf0e1fa21d90f8cea434fd30adce7db281449c30
DanielGS1572 <gonzalisd@gmail.com>
2019-06-11 14:38:07 -0500

    appium

--- appium/src/main/java/com/appium/test/Basics.java
+++ appium/src/main/java/com/appium/test/Basics.java
@@ -10,10 +10,27 @@ import java.net.MalformedURLException;
 public class Basics extends Base {
     public static void main(String args[]) throws MalformedURLException {
 
-       AndroidDriver<AndroidElement> driver =  capabilities();
-       //Para ver todos los componentes de la aplicación se debe usar ui automation viewer que esta dentro de /Android/Sdk/tools/bin
-        driver.findElementByAndroidUIAutomator("new UiSelector().text(\"Número de Empleado\")").sendKeys("1113");
-        driver.findElementByAndroidUIAutomator("new UiSelector().text(\"INGRESAR\")").click();
+        AndroidDriver<AndroidElement> driver = capabilities();
+        //Para ver todos los componentes de la aplicación se debe usar ui automation viewer que esta dentro de /Android/Sdk/tools/bin
+        for (int i = 0; i <= 2; i++) {
+            driver.findElementByAndroidUIAutomator("new UiSelector().text(\"Número de Empleado\")").sendKeys("1113");
+            driver.findElementByAndroidUIAutomator("new UiSelector().text(\"INGRESAR\")").click();
+
+
+            driver.findElement(By.xpath("//android.widget.LinearLayout[@index='1']//android.widget.ImageView[@index='1']")).click();
+            driver.findElementByAndroidUIAutomator("new UiSelector().text(\"CONTINUAR\")").click();
+            driver.findElementByAndroidUIAutomator("new UiSelector().text(\"CARGAR\")").click();
+            driver.findElementByAndroidUIAutomator("new UiSelector().text(\"FULL\")").click();
+            try {
+                Thread.sleep(2000);
+                driver.findElementByAndroidUIAutomator("new UiSelector().text(\"OK\")").click();
+                Thread.sleep(2000);
+                driver.findElementByAndroidUIAutomator("new UiSelector().text(\"SALIR\")").click();
+
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
 
     }
 }

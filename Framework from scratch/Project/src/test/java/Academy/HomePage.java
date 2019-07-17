1ef353791b5b532b6b4f707c492e1382fd50e4f5
DanielGS1572 <gonzalisd@gmail.com>
2019-07-16 20:47:46 -0500

    screenshots

--- Framework from scratch/Project/src/test/java/Academy/HomePage.java
+++ Framework from scratch/Project/src/test/java/Academy/HomePage.java
@@ -1,6 +1,7 @@
 package Academy;
 
-import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
@@ -12,30 +13,38 @@ import pageObjects.LogingPage;
 import resources.Base;
 
 public class HomePage extends Base {
-
+	public static Logger log = LogManager.getLogger(Base.class.getName());
 	@BeforeTest
 	public void beforeTest(){
+		log.info("Inicializando driver");
 		initializaDriver();
 	}
 	
 	@AfterTest
 	public void afterTest(){
+		log.info("cerrando driver");
 		driver.close();
+		driver = null; //se asegura que no se creen nuevos drivers
 	}
 	
 	@Test(dataProvider = "provider")
 	public void homePageNavigation(String mail, String password) {
 		driver.get(url);		//Si se pone en BeforeTest por el data provider la segunda vez lo ejecutara
 		//en la pagina de login
+		log.info("Creando Landing page");
 		LandingPage landingPage = new LandingPage(driver);
 		WebDriverWait wdw = new WebDriverWait(driver, 30);
 		//wdw.until(ExpectedConditions.visibilityOfElementLocated(landingPage.getNoThanks())).click();
+		getScreenShot();
 		landingPage.getByCSSLogin().click();
-
+		log.info("Creando Login page");
 		LogingPage loginPage = new LogingPage(driver);
 		loginPage.getByEmail().sendKeys(mail);
+		getScreenShot();
 		loginPage.getByPassword().sendKeys(password);
+		getScreenShot();
 		loginPage.getByLogin().click();
+		getScreenShot();
 	}
 
 	@DataProvider
